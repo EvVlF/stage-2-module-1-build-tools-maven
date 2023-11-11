3e64927ff0f1919619710f301d2023cc8358046b
EvVlF <ev.vl.fedorov@gmail.com>
2023-11-11 08:32:10 +0400

    edit match positive numbers

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,6 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        return str.matches("\\d+") && !str.startsWith("-");
+        return str.matches("\\+?\\d+(?:\\.\\d+)?(?:/(?:\\d+(?:\\.\\d+)?)?)?") && !str.startsWith("-");
     }
 }
